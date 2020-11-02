d90d3b8c0d9a6c003d6d496db6735f7a737862c5
Parsa Jahanlou <67125878+vynleran@users.noreply.github.com>
2020-11-02 14:09:34 -0600

    Added Book.java

---
+++ Book.java
@@ -0,0 +1,58 @@
+
+public class Book {
+	private String title;
+	private String author;
+	private int status;
+	private int priority;
+	
+	public Book()
+	{
+		title = "";
+		author = "";
+	}
+	public Book(String t, String a, int s, int p)
+	{
+		title = t;
+		author = a;
+		status = s;
+		priority = p;
+		
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String t)
+	{
+		title  = t;
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	public void setAuthor(String a)
+	{
+		author = a;
+	}
+	public int getStatus()
+	{
+		return status;
+	}
+	public void setStatus(int s)
+	{
+		status = s;
+	}
+	public int getPriority()
+	{
+		return priority;
+	}
+	public void setPriorirty(int p)
+	{
+		priority = p;
+	}
+	
+	
+	
+	
+}
